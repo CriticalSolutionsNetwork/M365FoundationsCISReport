5f4217d264e5116ea30216dbcd9fbd19fd2b70bc
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2024-05-28 10:15:40 -0500

    fix: 7.3.1 aligned with test-template

--- source/tests/Test-DisallowInfectedFilesDownload.ps1
+++ source/tests/Test-DisallowInfectedFilesDownload.ps1
@@ -1,25 +1,43 @@
 function Test-DisallowInfectedFilesDownload {
     [CmdletBinding()]
     param (
-        # Define your parameters here
+        # Define your parameters here if needed
     )
 
     begin {
-        # Initialization code
+        # Dot source the class script if necessary
+        . .\source\Classes\CISAuditResult.ps1
 
-        $auditResult = [CISAuditResult]::new()
+        # Initialization code, if needed
     }
 
     process {
         # 7.3.1 (L2) Ensure Office 365 SharePoint infected files are disallowed for download
+
+        # Retrieve the SharePoint tenant configuration
         $SPOTenantDisallowInfectedFileDownload = Get-SPOTenant | Select-Object DisallowInfectedFileDownload
         $isDisallowInfectedFileDownloadEnabled = $SPOTenantDisallowInfectedFileDownload.DisallowInfectedFileDownload
 
-        # Populate the auditResult object with the required properties
+        # Prepare failure reasons and details based on compliance
+        $failureReasons = if (-not $isDisallowInfectedFileDownloadEnabled) {
+            "Downloading infected files is not disallowed."
+        }
+        else {
+            "N/A"
+        }
+
+        $details = if ($isDisallowInfectedFileDownloadEnabled) {
+            "DisallowInfectedFileDownload: True"
+        }
+        else {
+            "DisallowInfectedFileDownload: False"
+        }
+
+        # Create and populate the CISAuditResult object
+        $auditResult = [CISAuditResult]::new()
         $auditResult.CISControlVer = "v8"
         $auditResult.CISControl = "10.1"
         $auditResult.CISDescription = "Deploy and Maintain Anti-Malware Software"
-
         $auditResult.Rec = "7.3.1"
         $auditResult.ELevel = "E5"
         $auditResult.ProfileLevel = "L2"
@@ -27,15 +45,14 @@ function Test-DisallowInfectedFilesDownload {
         $auditResult.IG2 = $true
         $auditResult.IG3 = $true
         $auditResult.RecDescription = "Ensure Office 365 SharePoint infected files are disallowed for download"
-
         $auditResult.Result = $isDisallowInfectedFileDownloadEnabled
-        $auditResult.Details = "DisallowInfectedFileDownload: $($SPOTenantDisallowInfectedFileDownload.DisallowInfectedFileDownload)"
-        $auditResult.FailureReason = if (-not $isDisallowInfectedFileDownloadEnabled) { "Downloading infected files is not disallowed." } else { "N/A" }
+        $auditResult.Details = $details
+        $auditResult.FailureReason = $failureReasons
         $auditResult.Status = if ($isDisallowInfectedFileDownloadEnabled) { "Pass" } else { "Fail" }
     }
 
     end {
-        # Return auditResult
+        # Return the audit result
         return $auditResult
     }
 }
