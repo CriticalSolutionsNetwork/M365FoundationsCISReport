d1555e1d25dfb2d6af701a9d951d15a0bf8e1286
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2024-05-28 20:07:46 -0500

    docs: update help

--- helpers/Build-Help.ps1
+++ helpers/Build-Help.ps1
@@ -3,7 +3,7 @@ Import-Module .\output\module\M365FoundationsCISReport\*\*.psd1
 .\helpers\psDoc-master\src\psDoc.ps1 -moduleName M365FoundationsCISReport -outputDir ".\" -template ".\helpers\psDoc-master\src\out-markdown-template.ps1" -fileName ".\README.md"
 
 
-#<#
+<#
     $ver = "v0.1.2"
     git checkout main
     git pull origin main
