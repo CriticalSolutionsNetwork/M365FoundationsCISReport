f830bdf2f01dee3f6cbe6d476f486e16499dd05b
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2024-07-07 12:56:02 -0500

    docs: Update testfile

--- helpers/Build-Help.ps1
+++ helpers/Build-Help.ps1
@@ -4,7 +4,7 @@ Import-Module .\output\module\M365FoundationsCISReport\*\*.psd1
 
 
 <#
-    $ver = "v0.1.22"
+    $ver = "v0.1.23"
     git checkout main
     git pull origin main
     git tag -a $ver -m "Release version $ver refactor Update"
