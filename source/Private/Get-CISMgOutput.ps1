88f256642234cca7e77323856b9df26ecffd9b38
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2024-12-26 14:40:25 -0600

    add: test and call for 1.1.4

--- source/Private/Get-CISMgOutput.ps1
+++ source/Private/Get-CISMgOutput.ps1
@@ -58,10 +58,6 @@ function Get-CISMgOutput {
                         return $AdminRoleAssignmentsAndUsers
                     }
                 }
-                '1.1.4' {
-                    # 1.1.4 - MicrosoftGraphPlaceholder
-                    # Placeholder for Test-AdminAccountLicenses
-                }
                 '1.1.3' {
                     # Test-GlobalAdminsCount
                     # Step: Retrieve global admin role
@@ -69,6 +65,32 @@ function Get-CISMgOutput {
                     # Step: Retrieve global admin members
                     $globalAdmins = Get-MgDirectoryRoleMember -DirectoryRoleId $globalAdminRole.Id
                     return $globalAdmins
+                }
+                '1.1.4' {
+                    # 1.1.4 - MicrosoftGraphPlaceholder
+                    $DirectoryRoles = Get-MgDirectoryRole
+                    # Get privileged role IDs
+                    $PrivilegedRoles = $DirectoryRoles |
+                    Where-Object { $_.DisplayName -like '*Administrator*' -or $_.DisplayName -eq 'Global Reader' }
+                    # Get the members of these various roles
+                    $RoleMembers = $PrivilegedRoles | ForEach-Object { Get-MgDirectoryRoleMember -DirectoryRoleId $_.Id } |
+                    Select-Object Id -Unique
+                    # Retrieve details about the members in these roles
+                    $PrivilegedUsers = $RoleMembers | ForEach-Object {
+                        Get-MgUser -UserId $_.Id -Property UserPrincipalName, DisplayName, Id
+                    }
+                    $Report = [System.Collections.Generic.List[Object]]::new()
+                    foreach ($Admin in $PrivilegedUsers) {
+                        $License = $null
+                        $License = (Get-MgUserLicenseDetail -UserId $Admin.id).SkuPartNumber -join ', '
+                        $Object = [pscustomobject][ordered]@{
+                            DisplayName       = $Admin.DisplayName
+                            UserPrincipalName = $Admin.UserPrincipalName
+                            License           = $License
+                        }
+                        $Report.Add($Object)
+                    }
+                    return $Report
                 }
                 '1.2.1' {
                     # Test-ManagedApprovedPublicGroups
